83f304221c5add7dffc4c805b70b82c1e09ae1f1
Beiweiz1 <905841628@qq.com>
2022-02-27 21:25:14 +0800

    3.1

--- unit3/3.1.cpp
+++ unit3/3.1.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+const int MAXN = 100 + 10;
+
+int arr[MAXN];
+
+int main(){
+    //Key 210
+    int n;
+    while(scanf("%d", &n) != EOF){
+        for(int i = 0; i < n; ++i){
+            scanf("%d", &arr[i]);
+        }
+        sort(arr, arr + n);
+        for(int i = 0; i < n; ++i){
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
